e208dbf085bd20eebe8b957164c3f83648345e82
Yashasva Sharma <yashasva2001@gmail.com>
2023-07-14 07:11:32 +0530

    fix: joi validation for requests

--- src/middleware/schemas.js
+++ src/middleware/schemas.js
@@ -1,19 +1,19 @@
 const Joi = require('joi');
 
 const userCreationSchema = Joi.object({
-    name: Joi.string(),
-    userName: Joi.string().required(),
+    name: Joi.string().required(),
+    userName: Joi.string().required().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
-    role: Joi.string().required(),
+    role: Joi.string().valid('admin','user').required(),
 })
 
 const userLoginSchema = Joi.object({
-    userName: Joi.string().required(),
+    userName: Joi.string().required().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
 })
 
 const userDeletionSchema = Joi.object({
-    userName: Joi.string().required(),
+    userName: Joi.string().required().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
 })
 
